3380e6c04c00484b111e35fc4759ca9917001905
botniedata <botniedata@gmail.com>
2024-02-17 16:48:30 +0800

    create a simple ftplib connection

---
+++ test.py
@@ -0,0 +1,27 @@
+# Import FTP from ftplib
+from ftplib import FTP_TLS
+
+# Connection parameters
+ftpHost = 'localhost'
+ftpPort = 21
+ftpUsername = 'test'
+ftpPassword = 'admin'
+
+# FTP timeout
+ftp = FTP_TLS(timeout=30)
+
+# Connect to the FTP server
+ftp.connect(ftpHost, ftpPort)
+
+# Login credentials
+ftp.login(ftpUsername, ftpPassword)
+
+# Setup secure connection
+ftp.prot_p()
+
+# -->
+
+# Send quit
+ftp.quit()
+
+print("FTP Login Complete...")
